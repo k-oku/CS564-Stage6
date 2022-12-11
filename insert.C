a80edf63dd85fcb02329670ac3c8ee3f36cccfdf
Max <80323354+GuyBritish@users.noreply.github.com>
2022-12-10 18:42:18 -0600

    Add check for unspecified attributes

--- insert.C
+++ insert.C
@@ -38,24 +38,33 @@ const Status QU_Insert(const string &relation, const int attrCnt, const attrInfo
 	entry.length = entrySz;
 
 	// Copy attributes over
+	int mrk;
 	int tmpi = 0;
 	float tmpf = 0;
 	for (int i = 0; i <= relAttrCnt - 1; ++i) {
+		mrk = 0;
 		for (int j = 0; j <= attrCnt - 1; ++j) {
 			if (strcmp(attrs[i].attrName, attrList[j].attrName) == 0 && attrs[i].attrType == attrList[j].attrType) {
-				if (attrs[i].attrType == 0)
+				mrk = 1;
+				if (attrs[i].attrType == 0) {
 					memcpy(entryData + attrs[i].attrOffset, (char *)attrList[j].attrValue, attrs[i].attrLen);
+					break;
+				}
 				if (attrs[i].attrType == 1) {
 					tmpi = atoi((char *)attrList[j].attrValue);
 					memcpy(entryData + attrs[i].attrOffset, (char *)&tmpi, attrs[i].attrLen);
+					break;
 				}
 				if (attrs[i].attrType == 2) {
 					tmpf = atof((char *)attrList[j].attrValue);	
 					memcpy(entryData + attrs[i].attrOffset, (char *)&tmpf, attrs[i].attrLen);
-				}
-				break;
+					break;
+				} 				
+				return ATTRTYPEMISMATCH;
 			}
 		}
+		if (!mrk)
+			return ATTRTYPEMISMATCH;
 	}
 	// Insert
 	InsertFileScan rel(relation, status);
